e255f3de304bae4764f1dd81ed7705c082e1a7f8
ucar-tmeyer <tiffanym@ucar.edu>
2025-04-10 16:43:14 -0500

    Update the Unidata plugins to work with Java11
    - Restructured the directory to abide by the common, edex, feature, viz convention
    - Upgraded references of com.vividsolutions.jts.* packages to org.locationtech.jts.*
    - import javax.measure.quantity.VolumetricFlowRate is now org.openhab.core.library.dimension.VolumetricFlowRate - I had to go find and download the RPM, then extract the org.openhab.core-3.4.4-4.jar and import it into eclipse, and add it to the manifest.MF file
    - A number of other java updates that are mentioned here: https://github.com/Unidata/awips-unidata-builds/blob/6c9a3bf3a18dc708c72e6d0a9d9ff59e1b1c4068/linux/setup/docs/NWS_Transition_Docs/Java11UnitsChanges.txt
    - Some of these included changing:
    Re-importing a lot of the javax modules into the MANIFEST.MF files and changing the Java Execution Environment to JavaSE-11
    times --> multiply
    Using the following:
    - import si.uom.NonSI;
    - import si.uom.SI;
    - import systems.uom.common.USCustomary;

---
+++ common/edu.ucar.unidata.common.dataplugin.spc/src/edu/ucar/unidata/common/dataplugin/spc/SPCRecord.java
@@ -0,0 +1,188 @@
+package edu.ucar.unidata.common.dataplugin.spc;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import org.hibernate.annotations.Index;
+import org.hibernate.annotations.Type;
+
+import com.raytheon.uf.common.dataplugin.PluginDataObject;
+import com.raytheon.uf.common.dataplugin.annotations.DataURI;
+import com.raytheon.uf.common.geospatial.adapter.GeometryAdapter;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
+import org.locationtech.jts.geom.Geometry;
+
+/**
+ * 
+ * SPC Convective Outlook Record
+ * 
+ * SOFTWARE HISTORY
+ * Date         Ticket#     Engineer    Description
+ * ------------ ----------  ----------- --------------------------
+ * Mar 27, 2018             mjames@ucar  Initial creation
+ * 
+ * @author mjames
+ */
+
+@Entity
+@SequenceGenerator(initialValue = 1, name = PluginDataObject.ID_GEN, sequenceName = "spc_seq")
+@Table(name = SPCRecord.PLUGIN_NAME, uniqueConstraints = { @UniqueConstraint(columnNames = { "dataURI" }) })
+/*
+ * Both refTime and forecastTime are included in the refTimeIndex since
+ * forecastTime is unlikely to be used.
+ */
+@org.hibernate.annotations.Table(appliesTo = SPCRecord.PLUGIN_NAME, indexes = { 
+		@Index(name = "spc_refTimeIndex", columnNames = {
+        "refTime", "forecastTime" }) })
+@DynamicSerialize
+public class SPCRecord extends PluginDataObject {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final String[] TORNADO_OUTLOOK = {
+			"2 %",
+			"5 %",
+			"10 %",
+			"15 %",
+			"30 %",
+			"45 %",
+			"60 %",
+			"Sig"
+	};
+	
+	public static final String[] HAILWIND_OUTLOOK = {
+			"5 %",
+			"15 %",
+			"30 %",
+			"45 %",
+			"60 %",
+			"Sig"
+	};
+		
+	public static final String[] TSTORM_OUTLOOK = {
+			"10 %",
+			"40 %",
+			"70 %"
+	};
+	
+	private static final String[] convectiveCategory= { 
+			"TSTM", 
+			"MRGL", 
+			"SLGT", 
+			"ENH", 
+			"MDT", 
+			"HIGH"
+	};
+
+	private static final String[] convectiveCategoryName = { 
+			"General Thunder",
+			"Marginal Risk",
+			"Slight Risk",
+			"Enhanced Risk",
+			"Moderate Risk",
+			"High Risk"
+	};
+	
+	public static Map<String, String> CONVECTIVE_OUTLOOK = 
+			mapDefinitions(convectiveCategoryName, convectiveCategory);
+
+	public static final String PLUGIN_NAME = "spc";
+
+	// report type
+	@Column(length = 32)
+	@DynamicSerializeElement
+	@DataURI(position = 1)
+	String reportType;
+
+	// report name
+	@Column(length = 32)
+	@DynamicSerializeElement
+	@DataURI(position = 2)
+	String typeCategory;
+	
+	// report part
+	@Column
+    @DynamicSerializeElement
+    @DataURI(position = 3)
+    Integer reportPart;
+
+	@Column(name = "location", columnDefinition = "geometry")
+	@Type(type = "org.hibernate.spatial.GeometryType")
+	@XmlJavaTypeAdapter(value = GeometryAdapter.class)
+	@DynamicSerializeElement
+	private Geometry geometry;
+
+	/**
+     * Default Constructor
+     */
+    public SPCRecord() {
+        this.reportType = SPCRecord.PLUGIN_NAME;
+        this.typeCategory = null;
+        this.geometry = null;
+    }
+
+	public Geometry getGeometry() {
+		return geometry;
+	}
+
+	public void setGeometry(Geometry geometry) {
+		this.geometry = geometry;
+	}
+
+	public void setReportPart(Integer part) {
+		this.reportPart = part;
+	}
+	
+	public Integer getReportPart() {
+		return reportPart;
+	}
+
+	public String getTypeCategory() {
+		return typeCategory;
+	}
+
+	public void setTypeCategory(String typeCategory) {
+		this.typeCategory = typeCategory;
+	}
+
+	public String getReportType() {
+		return reportType;
+	}
+
+	public void setReportType(String reportType) {
+		this.reportType = reportType;
+	}
+
+    @Override
+    @Column
+    @Access(AccessType.PROPERTY)
+    public String getDataURI() {
+        return super.getDataURI();
+    }
+	
+	@Override
+	public String getPluginName() {
+		return PLUGIN_NAME;
+	}
+	
+	public static Map<String, String> mapDefinitions(String[] s1, String[] s2) {
+		Map<String, String> mapping = new LinkedHashMap<>();
+		int i=0;
+		for (String name : s1 ) {
+			mapping.put(name, s2[i]);
+			i++;
+		}
+		return mapping;
+	}
+
+}
---
+++ edex/edu.ucar.unidata.edex.plugin.spc/src/edu/ucar/unidata/edex/plugin/spc/SPCDecoder.java
@@ -0,0 +1,189 @@
+package edu.ucar.unidata.edex.plugin.spc;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.joda.time.DateTime;
+
+import com.raytheon.uf.common.dataplugin.PluginDataObject;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.common.time.DataTime;
+import org.locationtech.jts.geom.GeometryFactory;
+
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Feature;
+import de.micromata.opengis.kml.v_2_2_0.Folder;
+import de.micromata.opengis.kml.v_2_2_0.Geometry;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.Coordinate;
+import de.micromata.opengis.kml.v_2_2_0.LinearRing;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.Polygon;
+import de.micromata.opengis.kml.v_2_2_0.TimeSpan;
+import edu.ucar.unidata.common.dataplugin.spc.SPCRecord;
+
+/**
+ * 
+ * Decoder for SPC convective outlook KML files.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Engineer    Description
+ * ------------ ----------- --------------------------
+ * Mar 27, 2018 mjames      Initial creation
+ * Mar 29, 2018 mjames      Smoothing by coordinate sampling
+ * 
+ * </pre>
+ *
+ * @author mjames@ucar.edu
+ */
+
+public class SPCDecoder {
+
+	GeometryFactory geomFact = new GeometryFactory();
+
+	private IUFStatusHandler logger = UFStatus.getHandler(SPCDecoder.class);    
+
+	private Map<String, String> reportType = SPCRecord.mapDefinitions(report, reportRegex);
+
+	private static final String[] report = { 
+			"Convective Outlook", 
+			"Tornado Outlook",
+			"Wind Outlook",
+			"Hail Outlook"
+			//,"Thunderstorm Outlook"
+	};
+	private static final String[] reportRegex = { 
+			"cat", 
+			"torn", 
+			"wind",
+			"hail"
+			//,"enh"
+	};
+	
+	public PluginDataObject[] decode(byte[] data) throws Exception {
+
+		ArrayList<SPCRecord> list = new ArrayList<SPCRecord>();
+
+		// Modification needed for unmarshaling
+		String input = new String(data).replace("xmlns=\"http://earth.google.com/kml/2.2\"", 
+				"xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\"" );
+
+		ByteArrayInputStream stream = new ByteArrayInputStream( input.getBytes( "UTF-8" ) );
+		
+		try {
+			Document document = (Document) Kml.unmarshal(stream).getFeature();
+			List<Feature> folders = document.getFeature();
+	
+			for(Feature feature : folders) {
+				
+				String folderName = feature.getName();
+				String type = null;
+				for (Entry<String, String> typ : reportType.entrySet()) {
+					if (folderName.endsWith(typ.getValue())) {
+						type = typ.getKey();
+						break;
+					}
+				}
+
+				if (feature instanceof Folder) {
+					
+					Folder folder = (Folder) feature;
+					List<Feature> placemarkList = folder.getFeature();
+					int reportCount = 0;
+					
+					for (Feature mark : placemarkList ) {
+						reportCount++;
+						Placemark placemark = (Placemark) mark;
+						String category = placemark.getName();
+						Geometry geometry = placemark.getGeometry();
+						
+						if(geometry instanceof Polygon) {
+							
+							Polygon polygon = (Polygon) geometry;
+							
+							TimeSpan timePrimitive = (TimeSpan) placemark.getTimePrimitive();
+							String dateString = timePrimitive.getBegin();
+							Date date = new DateTime(dateString).toDate();
+							DataTime dataTime = new DataTime(date);
+							
+							try {
+								SPCRecord record = new SPCRecord();
+								record.setDataTime(dataTime);
+								record.setTypeCategory(category);
+								record.setReportType(type);
+								record.setReportPart(reportCount);
+								record.setGeometry(geomFact.createPolygon(
+										modifyLinearRing(polygon.getOuterBoundaryIs().getLinearRing())));
+								list.add(record);
+							} catch (Exception ex) {
+								ex.printStackTrace();
+							}
+						}
+					}
+					logger.info(placemarkList.size() + " polygons processed for KML feature " + folderName.toString() );
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		PluginDataObject[] decodedData = list.toArray(new PluginDataObject[list.size()]);
+		return (decodedData);
+	}
+
+	public org.locationtech.jts.geom.LinearRing modifyLinearRing(LinearRing l) {
+		return geomFact.createLinearRing(this.convertCoords(l.getCoordinates()));
+	}
+	
+	/**
+	 * 
+	 * Convert Coordinate list from KML to JTS
+	 * 
+	 * @param geomList
+	 * @return
+	 */
+	public org.locationtech.jts.geom.Coordinate[] convertCoords(List<Coordinate> geomList) {
+
+		if (1 == 0 && geomList.size() > 40) {
+			
+			// Downsample if large DISABLED
+			int inc = 4;
+			int limit = Math.round(geomList.size()/inc);
+			org.locationtech.jts.geom.Coordinate[] coordList = new org.locationtech.jts.geom.Coordinate[limit];
+			for (int i = 0; i < coordList.length; i++) {
+				org.locationtech.jts.geom.Coordinate b = new org.locationtech.jts.geom.Coordinate();
+				b.x = geomList.get(i*inc).getLongitude();
+				b.y = geomList.get(i*inc).getLatitude();
+				coordList[i] = b;
+			}
+			// Ensure last coordinates are equal for a valid closed LinearRing
+			coordList[coordList.length-1].x = geomList.get(geomList.size()-1).getLongitude();
+			coordList[coordList.length-1].y = geomList.get(geomList.size()-1).getLatitude();
+			return coordList;
+			
+		} else {
+			
+			int j = 0;
+			org.locationtech.jts.geom.Coordinate[] coordList = new org.locationtech.jts.geom.Coordinate[geomList.size()];
+			for (Coordinate i : geomList) {
+	            org.locationtech.jts.geom.Coordinate b = new org.locationtech.jts.geom.Coordinate();
+				b.x = i.getLongitude();
+				b.y = i.getLatitude();
+				coordList[j] = b;
+				j = j + 1;
+			}
+			return coordList;
+			
+		}
+	}
+}
